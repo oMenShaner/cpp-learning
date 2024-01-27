873863e2ce29f7c75ad08e218869e293d80cb083
oMen_shaner <oMen_shaner@163.com>
2024-01-27 23:45:49 +0800

    List实现

---
+++ List/list.h
@@ -0,0 +1,242 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+#include <assert.h>
+
+namespace wr
+{
+  // ListNode
+  template <typename T>
+  struct ListNode
+  {
+    ListNode<T> *_next; // 指向后继结点的指针
+    ListNode<T> *_prev; // 指向前驱结点的指针
+    T _data;            // 存放结点的数据
+
+    ListNode(const T &val = T()) // 全缺省构造
+        : _next(nullptr), _prev(nullptr), _data(val)
+    {
+    }
+  };
+
+  // __list_iterator
+  template <typename T, typename Ref, typename Ptr>
+  struct __list_iterator
+  {
+    typedef ListNode<T> Node;
+    typedef __list_iterator<T, Ref, Ptr> self; // 重命名为self
+
+    Node *_node; // 迭代器指向的结点指针
+
+    __list_iterator(Node *node = nullptr)
+        : _node(node)
+    {
+    }
+    __list_iterator(const self &s)
+        : _node(s._node)
+    {
+    }
+
+    self &operator++()
+    {
+      _node = _node->_next;
+      return *this;
+    }
+    self operator++(int)
+    {
+      self tmp(*this);
+      _node = _node->_next;
+      return tmp;
+    }
+    self &operator--()
+    {
+      _node = _node->_prev;
+      return *this;
+    }
+    self operator--(int)
+    {
+      self tmp(*this);
+      _node = _node->_prev;
+      return tmp;
+    }
+    Ref operator*()
+    {
+      return _node->_data;
+    }
+    Ptr operator->()
+    {
+      return &(operator*());
+    }
+
+    bool operator!=(const self &s)
+    {
+      return _node != s._node;
+    }
+    bool operator==(const self &s)
+    {
+      return _node == s._node;
+    }
+  };
+
+  // list
+  template <typename T>
+  class list
+  {
+    typedef ListNode<T> Node;
+
+  public:
+    typedef __list_iterator<T, T &, T *> iterator;
+    typedef __list_iterator<T, const T &, const T *> const_iterator;
+
+    list()
+    {
+      empty_init();
+    }
+    list(int n, const T &val = T())
+    {
+      empty_init();
+      for (int i = 0; i < n; ++i)
+      {
+        push_back(val);
+      }
+    }
+
+    ~list()
+    {
+      clear();
+      delete _head;
+      _head = nullptr;
+    }
+
+    ////////////////////////////////////////////////////////////
+    // List Iterator
+    iterator begin()
+    {
+      return _head->_next;
+    }
+    iterator end()
+    {
+      return _head;
+    }
+    const_iterator begin() const
+    {
+      return _head->_next;
+    }
+    const_iterator end() const
+    {
+      return _head;
+    }
+    
+    ////////////////////////////////////////////////////////////
+    // List Capacity
+    size_t size() const
+    {
+      size_t size = 0;
+      iterator it = begin();
+      while (it != end())
+      {
+        ++size;
+        ++it;
+      }
+      return size;
+    }
+    bool empty() const
+    {
+      return size();
+    }
+
+    ////////////////////////////////////////////////////////////
+    // List Access
+    T &front()
+    {
+      return *(begin());
+    }
+    const T &front() const
+    {
+      return *(begin());
+    }
+    T &back()
+    {
+      return *(--end());
+    }
+    const T &back() const
+    {
+      return *(--end());
+    }
+
+    ////////////////////////////////////////////////////////////
+    // List Modify
+    void push_back(const T &val = T())
+    {
+      // Node *newNode = new Node(val);
+      // Node *tail = _head->_prev;
+
+      // tail->_next = newNode;
+      // newNode->_prev = tail;
+      // newNode->_next = _head;
+      // _head->_prev = newNode;
+      insert(end(), val);
+    }
+    void pop_back()
+    {
+      erase(--end());
+    }
+    void push_front(const T &val = T())
+    {
+      insert(begin(), val);
+    }
+    void pop_front()
+    {
+      erase(begin());
+    }
+    iterator insert(iterator pos, const T &val)
+    { // 在pos位置前插入值为val的节点
+      Node *cur = pos._node;
+      Node *prev = cur->_prev;
+      Node *newNode = new Node(val);
+
+      prev->_next = newNode;
+      newNode->_prev = prev;
+      newNode->_next = cur;
+      cur->_prev = newNode;
+
+      return newNode;
+    }
+    iterator erase(iterator pos)
+    { // 删除pos位置的节点，返回该节点的下一个位置
+      assert(pos != end());
+
+      Node *cur = pos._node;
+      Node *prev = cur->_prev;
+      Node *next = cur->_next;
+      prev->_next = next;
+      next->_prev = prev;
+
+      delete cur;
+
+      return next;
+    }
+    void clear()
+    {
+      iterator it = begin();
+      while (it != end())
+      {
+        it = erase(it);
+      }
+    }
+    void swap(list<T> &l)
+    {
+      std::swap(_head, l._head);
+    }
+
+  private:
+    void empty_init()
+    {
+      _head = new Node;
+      _head->_next = _head;
+      _head->_prev = _head;
+    }
+    Node *_head;
+  };
+}
+
+#endif // __LIST_H__
---
+++ List/test.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <utility>
+#include <string>
+#include "list.h"
+
+using namespace std;
+using namespace wr;
+
+#define SHOW(x)       \
+  for (auto e : x)    \
+    cout << e << " "; \
+  cout << endl;       \
+
+void Test1()
+{
+  list<int> l;
+  l.push_back(1);
+  l.push_back(2);
+  l.push_back(3);
+  l.push_back(4);
+  l.push_back(5);
+
+  list<int>::iterator lt = l.begin();
+  while (lt != l.end())
+  {
+    cout << *lt << " ";
+    lt++;
+  }
+  cout << endl;
+}
+
+void Test2()
+{
+  list<int> l;
+  l.push_back(1);
+  l.push_back(2);
+  l.push_back(3);
+  l.push_back(4);
+  l.push_back(5);
+  l.push_back(6);
+  SHOW(l);
+
+  l.push_front(0);
+  SHOW(l);
+  l.pop_back();
+  SHOW(l);
+  l.pop_front();
+  SHOW(l);
+  l.clear();
+  SHOW(l);
+}
+
+void Test3()
+{
+  list<string> l1;
+  l1.push_back("1111");
+  l1.push_back("2222");
+  l1.push_back("3333");
+  l1.push_back("4444");
+  l1.push_back("5555");
+  l1.push_back("6666");
+  SHOW(l1);
+
+  list<string> l2;
+  l2.push_back("aaaa");
+  l2.push_back("bbbb");
+  l2.push_back("cccc");
+  l2.push_back("dddd");
+  l2.push_back("eeee");
+  SHOW(l2);
+
+  l1.swap(l2);
+  SHOW(l1);
+
+  l1.front() = "1111";
+  l1.back() = "9999";
+  cout << l1.front() << endl;
+  cout << l1.back() << endl;
+  SHOW(l1);
+}
+
+void Test4()
+{
+  list<int> l;
+  cout << l.empty() << endl;
+  cout << l.size() << endl;
+
+  l.push_back(1);
+  l.push_back(1);
+  l.push_back(1);
+  l.push_back(1);
+  l.push_back(1);
+  l.push_back(1);
+  l.push_back(1);
+  cout << l.empty() << endl;
+  cout << l.size() << endl;
+}
+int main()
+{
+  //Test1();
+  //Test2();
+  //Test3();
+  Test4();
+
+  return 0;
+}
